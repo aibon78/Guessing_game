d2bc246731d9953cb65793bf95ed79d385ca647a
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-06-28 16:10:30 -0300

    Create Jogo da adivinhação.py

---
+++ Jogo da adivinhação.py
@@ -0,0 +1,23 @@
+print("*********************************")
+print("Bem vindo ao jogo da Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute =int(input("Digite o seu numero"))
+
+if numero_secreto == chute:
+    print("Você acertou")
+else:
+    print("Você errou")
+    
+if numero_secreto == chute:
+    print("!!Muito bom!!")
+else:
+    print("Mandou mal")
+
+if numero_secreto == chute:
+    print("You Win")
+else:
+    print("Game Over")
+
